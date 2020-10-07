a73a84e98e9dbc93693b6bf6c98dcf5de8d40fe7
Vithika Jha <vithikajha14@gmail.com>
2020-10-07 14:36:12 +0530

    config prettier & install materialize

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,12 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import M from 'materialize-css'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  created() {
+    M.AutoInit()
   }
 }
-</style>
+</script>
